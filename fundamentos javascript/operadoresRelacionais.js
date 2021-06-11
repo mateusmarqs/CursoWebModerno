d42a6e459520beec88fdcb9d9a9736e21d8383cb
mateusmarqs <mateusmarquesdc@gmail.com>
2021-06-10 23:33:09 -0300

    Aulas 50 e 51

---
+++ fundamentos javascript/operadoresRelacionais.js
@@ -0,0 +1,13 @@
+console.log('1' == 1) //Retorna verdadeiro
+console.log('1' === 1) //Retorna falso, === é restritamente sonre o tipo
+console.log('1' != 1) 
+console.log('1' !== 1) 
+
+console.log(2 > 1)
+console.log(2 < 1) 
+console.log(2 >= 1)
+console.log(2 <= 1)
+
+console.log(undefined == null)
+console.log(undefined === null)
+
